6242ada8ac8f3c539c38e040d8eb6cd27a62f995
652175qwasZX <andreidiving@gmail.com>
2020-08-15 03:48:44 +0800

    Работа над адаптивностью

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
+$(document).ready(function(){
+	$('#nav-icon2').click(function(){
+		$(this).toggleClass('open');
+	});
 });
